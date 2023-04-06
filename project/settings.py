88f8de14d3f0fb245f53395ec529a05b64c6e687
bekzodbek2006 <bekzodbekk2006@gmail.com>
2023-04-06 15:08:47 +0500

    update

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -111,3 +112,10 @@ STATICFILES_DIRS = ['static']
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
