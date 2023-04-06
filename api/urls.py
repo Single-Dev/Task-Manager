b23998cfc848edc54f7a23d4f7626198eb89925b
bekzodbek2006 <bekzodbekk2006@gmail.com>
2023-04-06 15:26:53 +0500

    make it better

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
 
-urlpatterns =[
-    path('', )
+urlpatterns = [
+  path("get-details",UserDetailAPI.as_view()),
+  path('register',RegisterUserAPIView.as_view()),
 ]
--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework.authtoken import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('api.urls'))
+    path('', include('api.urls')),
+    path('api-token-auth', views.obtain_auth_token)
 ]
