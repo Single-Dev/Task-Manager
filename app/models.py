7a7183388932ae23cd645f87d398519eb0c6e78c
bekzodbek2006 <bekzodbekk2006@gmail.com>
2023-04-25 21:46:06 +0500

    `ToDo model update` added owner

--- app/models.py
+++ app/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class ToDo(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     caption = models.TextField(max_length=200, blank=True)
     done = models.BooleanField(default=False)
