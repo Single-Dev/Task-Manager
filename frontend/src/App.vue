82dc7b5051b0c0ab951cf68a1eb44206047f6c80
bekzodbek2006 <bekzodbekk2006@gmail.com>
2023-06-11 11:11:27 +0500

    make it better

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,7 +7,10 @@
     </div>
     <button v-if="IsAuthenticated === true" @click="logout">Logout</button>
   </nav>
-  <router-view />
+  <div>
+    {{ username }}
+  </div>
+  <router-view :user_id="user_id" :tasks="tasks"/>
 </template>
 
 <script>
@@ -19,6 +22,10 @@ export default {
   data() {
     return {
       IsAuthenticated: false,
+      username: '',
+      user_id: '',
+      isLoading: false,
+      tasks: []
     }
   },
   methods: {
@@ -31,21 +38,60 @@ export default {
       } else {
         axios.defaults.headers.common['Authorization'] = ''
       }
+      this.getMe()
+      this.getTasks()
+    },
+    getMe() {
+      axios.get('/api/v1/users/me/',)
+        .then(response => {
+          this.username = response.data.username
+          this.user_id = response.data.id
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    },
+    async getTasks() {
+      try {
+        this.isLoading = true
+        const response = await axios.get('/api/tasks/',)
+        const newArr = response.data.map(item => ({
+          id: item.id,
+          name: item.name,
+          caption: item.caption,
+          done: item.done,
+          created_on: item.created_on,
+          reminder: item.reminder,
+          owner: item.owner
+        }))
+        this.tasks = newArr
+        console.log(this.tasks);
+      } catch (error) {
+        alert(error.message)
+      } finally {
+        this.isLoading = false
+      }
     },
     async logout(){
       try {
-        const response = await axios.post('http://127.0.0.1:8000/api/v1/token/logout/')
+        const response = await axios.post('/api/v1/token/logout/')
         localStorage.removeItem('token')
         axios.defaults.headers.common['Authorization'] = ''
-        this.$router.push('/')
         console.log(response);
       } catch (error) {
         alert(error.message)
+      } finally{
+        this.beforeCrete()
+        this.getMe()
+        this.getTasks()
       }
     }
   },
   mounted() {
     this.beforeCrete()
+    this.getMe()
+    this.getTasks()
   },
 }
 
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,63 +1,25 @@
 <template>
   <div class="home">
-    {{ username }}
     <todo :user_id="user_id" :tasks="tasks"/>
   </div>
 </template>
 
 <script>
 import todo from '@/components/todo.vue'
-import axios from 'axios'
 export default {
   name: 'HomeView',
-  data() {
-    return {
-      username: '',
-      user_id: '',
-      isLoading: false,
-      tasks: []
-    }
-  },
   components: {
     todo
   },
-  methods: {
-    getMe() {
-      axios.get('/api/v1/users/me/',)
-        .then(response => {
-          this.username = response.data.username
-          this.user_id = response.data.id
-          console.log(response.data);
-        })
-        .catch(error => {
-          console.log(error);
-        })
+  props:{
+    user_id:{
+      type:Number,
+      required: true
     },
-    async getTasks() {
-      try {
-        this.isLoading = true
-        const response = await axios.get('/api/tasks/',)
-        const newArr = response.data.map(item => ({
-          id: item.id,
-          name: item.name,
-          caption: item.caption,
-          done: item.done,
-          created_on: item.created_on,
-          reminder: item.reminder,
-          owner: item.owner
-        }))
-        this.tasks = newArr
-        console.log(this.tasks);
-      } catch (error) {
-        alert(error.message)
-      } finally {
-        this.isLoading = false
-      }
+    tasks:{
+      type: Array,
+      required: true
     },
-  },
-  mounted() {
-    this.getMe()
-    this.getTasks()
-  },
+  }
 }
 </script>
