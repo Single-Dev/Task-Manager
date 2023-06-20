b09d110b9f10ecc12c377e71a979f632cc65d835
bekzodbek2006 <bekzodbekk2006@gmail.com>
2023-06-20 17:05:21 +0500

    minor changes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,22 +2,25 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SignUp from '../views/SignUp.vue'
 import Login from '../views/Login.vue'
+import SharredTasks from '../views/SharredTasks.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
-  },
-  {
+  },{
     path: '/signup',
     name: 'SignuUp',
     component: SignUp
-  },
-  {
+  },{
     path: '/login',
     name: 'Login',
     component: Login
+  },{
+    path: '/sharred-tasks',
+    name: 'SharredTasks',
+    component: SharredTasks
   },
 ]
 
---
+++ frontend/src/views/SharedTasks.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Sharred</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'SharredTasks'
+}
+</script>
+<style lang="">
+    
+</style>
